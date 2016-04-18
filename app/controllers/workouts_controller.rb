d032ee3db50c0d8ece3d29afeb5eb49fdf7204da
Joe Spampinato <jcspampinato@gmail.com>
2016-04-18 16:05:31 -0400

    Added app gems, workout forms, views

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,39 @@
 class WorkoutsController < ApplicationController
+
+	before_action :find_workout, only: [:show, :edit, :update, :destroy]
+	def create
+		@workout = Workout.new(workout_params)
+		if @workout.save
+			redirect_to @workout
+		else
+			render 'new'
+		end
+	end
+
+	def destroy
+	end
+
 	def index
 		
+	end
+
+	def new
+		@workout = Workout.new
+	end
+
+	def show
+	end
+
+	def update 
+	end
+
+	private
+
+	def workout_params
+		params.require(:workout).permit(:date, :workout, :mood, :length)
+	end
+
+	def find_workout
+		@workout = Workout.find(params[:id])
 	end
 end
